b4cd758208d3a35f348a5896588e4ae0c8d77280
Steven Lippert <slippert@2-D-4.42heilbronn.de>
2024-02-24 12:45:38 +0100

    addet: send msg to clients

--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: slippert <slippert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:04:38 by slippert          #+#    #+#             */
-/*   Updated: 2024/02/23 18:05:45 by slippert         ###   ########.fr       */
+/*   Updated: 2024/02/24 12:40:29 by slippert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,24 @@ Server::~Server()
 
 void Server::start()
 {
+	srvInit();
+	srvLstn();
+}
+
+void Server::srvInit()
+{
+	funnyUsernames.push("Fröhlicher Frosch");
+	funnyUsernames.push("Tanzende Tomate");
+	funnyUsernames.push("Kichernder Kranich");
+	funnyUsernames.push("Lustige Libelle");
+	funnyUsernames.push("Strahlende Schildkröte");
+	funnyUsernames.push("Witzige Wurzel");
+	funnyUsernames.push("Kichernde Kaktus");
+	funnyUsernames.push("Tanzende Gurke");
+	funnyUsernames.push("Lachender Wal");
+	funnyUsernames.push("Trockener Humor");
+	funnyUsernames.push("Gewürzgurke97");
+
 	if ((_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		throw(std::runtime_error("Error: socket"));
 
@@ -30,47 +48,69 @@ void Server::start()
 	_ServerAddress.sin_addr.s_addr = INADDR_ANY;
 	_ServerAddress.sin_port = htons(_port);
 
-	if ((_bind = bind(_socket, (struct sockaddr *)&_ServerAddress, sizeof(_ServerAddress))) == -1)
+	int en = 1;
+	if (setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1)
+		throw(std::runtime_error("Error: setsockopt"));
+
+	if ((bind(_socket, (struct sockaddr *)&_ServerAddress, sizeof(_ServerAddress))) == -1)
 		throw(std::runtime_error("Error: bind"));
 
-	if ((_listen = listen(_socket, _maxConnections)) == -1)
+	if (fcntl(_socket, F_SETFL, O_NONBLOCK) == -1)
+		throw(std::runtime_error("Error: fcntl"));
+
+	if ((listen(_socket, _maxConnections)) == -1)
 		throw(std::runtime_error("Error: listen"));
+}
 
-	std::cout << "~ Server is running... ~\n" << std::endl;
-
-	int clients = 0;
+void Server::srvLstn()
+{
+	std::cout << blue << "~ Server is running ~ " << res << std::endl;
+	std::cout << std::endl;
+	fd_set read_set;
+	struct timeval timeout;
 
 	while (1)
 	{
-		_ClientAddrLen = sizeof(_ClientAddress);
-		if ((_accept = accept(_socket, (struct sockaddr *)&_ClientAddress, &_ClientAddrLen)) == -1)
-			throw(std::runtime_error("Error: accept"));
-		clients++;
-		User newUser;
-		newUser.id = _accept;
-		newUser.username = itoa(clients);
-		Clients[_accept] = newUser;
-		std::cout << "Client " << clients << " joined the channel" << std::endl;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 10000;
+		FD_ZERO(&read_set);
+		FD_SET(_socket, &read_set);
 
-		pthread_t client_thread;
-		pthread_create(&client_thread, NULL, handle_client, (void *)&Clients[_accept]);
-		pthread_detach(client_thread);
+		int ret;
+		if ((ret = select(_socket + 1, &read_set, NULL, NULL, &timeout)) == -1)
+			throw(std::runtime_error("Error: select"));
+		else if (ret == 0)
+		{
+		}
+		else
+		{
+			if (FD_ISSET(_socket, &read_set))
+			{
+				_ClientAddrLen = sizeof(_ClientAddress);
+				int _clientSocket;
+				if ((_clientSocket = accept(_socket, (struct sockaddr *)&_ClientAddress, &_ClientAddrLen)) == -1)
+					throw(std::runtime_error("Error: accept"));
+				userJoined(_clientSocket);
+			}
+		}
+		recvSignal();
+		sendSignal();
 	}
 }
 
-void *Server::handle_client(void *arg)
+void Server::recvSignal()
 {
-	User user = *((User *)arg);
-	while (1)
+	for (_itClient = Clients.begin(); _itClient != Clients.end(); _itClient++)
 	{
 		char buffer[1024];
-		ssize_t bytes_received = recv(user.id, buffer, sizeof(buffer), 0);
+		ssize_t bytes_received = recv(_itClient->second.socket, buffer, sizeof(buffer), 0);
 		if (bytes_received == -1)
-			throw(std::runtime_error("Error: recv"));
+			continue;
 		else if (bytes_received == 0)
 		{
-			std::cout << user.username << " disconnected." << std::endl;
-			break;
+			std::cout << blue + _itClient->second.username + white + " disconnected." << std::endl;
+			close(_itClient->second.socket);
+			_itClient->second.socket = -1;
 		}
 
 		buffer[bytes_received] = '\0';
@@ -82,17 +122,48 @@ void *Server::handle_client(void *arg)
 		if (commands.size() == 2)
 		{
 			if (commands[0] == "NICK")
-				user.username = commands[1];
+			{
+				std::cout << blue << "Server ➤ " << white << "User: " << _itClient->second.username << res << " has changed his name to ";
+				_itClient->second.username = commands[1];
+				std::cout << white << _itClient->second.username << res << std::endl;
+				continue;
+			}
 		}
-
-		std::vector<std::string>::iterator it;
-		std::cout << user.username << ": ";
-		for (it = commands.begin(); it != commands.end(); it++)
-			std::cout << *it << " ";
-		std::cout << std::endl;
+		std::string userPrefix = green + _itClient->second.username + " ➤ " + res;
+		UserInfo info;
+		info.socket = _itClient->second.socket;
+		info.username = _itClient->second.username;
+		info.message = userPrefix + std::string(buffer);
+		Messages[_itClient->second.socket] = info;
 	}
-	close(user.id);
-	return NULL;
+}
+
+void Server::sendSignal()
+{
+	for (_itMessages = Messages.begin(); _itMessages != Messages.end(); _itMessages++)
+	{
+		for (_itReceiver = Clients.begin(); _itReceiver != Clients.end(); _itReceiver++)
+		{
+			if (_itMessages->second.socket == _itReceiver->second.socket)
+				continue;
+			ssize_t bytes_send = send(_itReceiver->second.socket, _itMessages->second.message.c_str(), _itMessages->second.message.size(), 0);
+			if (bytes_send == -1)
+				throw(std::runtime_error("Error: send"));
+		}
+	}
+	Messages.clear();
+}
+
+void Server::userJoined(int clientSocket)
+{
+	UserInfo newUser;
+	newUser.socket = clientSocket;
+	newUser.username = funnyUsernames.top();
+	newUser.message = blue + newUser.username + green + " joined the channel\n" + res;
+	Clients[clientSocket] = newUser;
+	Messages[clientSocket] = newUser;
+	funnyUsernames.pop();
+	std::cout << newUser.message;
 }
 
 std::string Server::itoa(int i)
--- Server.hpp
+++ Server.hpp
@@ -6,7 +6,7 @@
 /*   By: slippert <slippert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:04:42 by slippert          #+#    #+#             */
-/*   Updated: 2024/02/23 18:04:43 by slippert         ###   ########.fr       */
+/*   Updated: 2024/02/24 12:31:05 by slippert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <poll.h>
 #include <unistd.h>
 #include <vector>
-#include <sstream>
+#include <stack>
 #include <map>
+#include <sstream>
 #include <algorithm>
+#include <fcntl.h>
+
+#define green "\033[1m\033[32m"
+#define blue "\033[1m\033[34m"
+#define white "\033[1m\033[37m"
+#define res "\033[0m"
+
+struct UserInfo
+{
+	int socket;
+	std::string username;
+	std::string message;
+};
 
 class Server
 {
 private:
+	typedef std::map<int, UserInfo>::iterator iter;
 	const int _port;
-	static const int _maxConnections = 1;
+	static const int _maxConnections = 128;
 
 	int _socket;
-	int _bind;
-	int _listen;
-	int _accept;
 
-	struct User
-	{
-		int id;
-		std::string username;
-	};
-	std::map<int, User> Clients;
+	std::map<int, UserInfo> Clients;
+	std::map<int, UserInfo> Messages;
+	std::stack<std::string> funnyUsernames;
+
+	iter _itClient;
+	iter _itReceiver;
+	iter _itMessages;
 
 	struct sockaddr_in _ServerAddress;
 	struct sockaddr_in _ClientAddress;
 	socklen_t _ClientAddrLen;
+
 	std::string itoa(int i);
-	static void *handle_client(void *arg);
 	static std::vector<std::string> split_buffer(const char *buffer);
 	static std::string trim_whitespace(std::string str);
+
+	void srvInit();
+	void srvLstn();
+	void userJoined(int clientSocket);
+	void recvSignal();
+	void sendSignal();
 
 public:
 	Server(int port);
